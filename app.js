0915a8deb852483692ef1ff542cadd5b744eec54
Irina Rakova <lis.ovskaya1119@gmail.com>
2022-08-23 17:48:24 +0300

    init commit

--- app.js
+++ app.js
@@ -1,12 +1,55 @@
 let ol = document.querySelector('#elem');
-let button = document.querySelector('#button');
+let addElem = document.querySelector('#button');
 let input = document.querySelector('#inp');
 
 
-button.addEventListener('click', function () {
+addElem.addEventListener('click', function addElement() {
 
     let li = document.createElement('li');
     li.textContent = input.value;
     ol.appendChild(li);
     input.value = ' ';
+
+    let button = document.createElement('button');
+    button.textContent = 'delete';
+    ol.appendChild(button);
+    button.classList.add('close');
+
+    button.addEventListener('click', function () {
+        li.remove();
+        button.remove();
+    })
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

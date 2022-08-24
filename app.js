7f099bc6178b50905de3aca0e6abcdf0b2f78e49
Irina Rakova <lis.ovskaya1119@gmail.com>
2022-08-24 17:34:04 +0300

    init commit

---
+++ app.js
@@ -0,0 +1,14 @@
+let table = document.querySelector('#table');
+let arr = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12]];
+
+for (let subArr of arr) {
+    let tr = document.createElement('tr');
+
+    for (let elem of subArr) {
+
+        let td = document.createElement('td');
+        td.textContent += Math.pow(elem, 2);
+        tr.appendChild(td);
+    }
+    table.appendChild(tr);
+}
